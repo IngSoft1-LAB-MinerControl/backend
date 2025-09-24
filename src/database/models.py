f1862d229cd7f8e87878e6626d18ac59b43e7112
Angelo Zoppetti <angelo@host133.psi.unc.edu.ar>
2025-09-24 09:19:11 -0300

    SCRUM-12 implementar nuevas tablas para secretos y cartas

--- src/database/models.py
+++ src/database/models.py
@@ -22,6 +22,25 @@ class Player(Base):
     birth_date = Column(Date, nullable = False)
     game_id = Column(Integer, ForeignKey("games.game_id"), nullable=False)  
     game = relationship("Game", back_populates="players")
+
+class Card(Base):
+    __tablename__ = 'cards'
+    card_id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(15))
+    picked_up = Column(Boolean)
+    dropped = Column(Boolean)
+    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)  
+    player = relationship("Players", back_populates="cards")
+
+class Secrets(Base):
+    __tablename__ = 'Secrets'
+    secret_id = Column(Integer, primary_key=True, autoincrement=True)
+    murderer = Column(Boolean)
+    acomplice = Column(Boolean)
+    revelated = Column(Boolean)
+    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)  
+    player = relationship("Players", back_populates="players")
+
     
    
 
