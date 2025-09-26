df792addd294e07ba7af12551aa1c28d9a5d7785
SantinoQuevedo <santino.quevedo@mi.unc.edu.ar>
2025-09-25 21:28:09 -0300

    SCRUM-16 añadido cors para que ande bien backend

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.routes.players_routes import player
 from src.routes.games_routes import game
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Podés poner la URL de tu frontend: ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],  # Authorization, Content-Type, etc.
+)
+
+
 @app.get("/")
-def hola() :
+def hola():
     return "Hola Mundo"
- 
+
+
 app.include_router(player)
 app.include_router(game)
