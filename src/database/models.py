8d25f5fa56a5326193c7d20741d77e2c7b6ca378
Angelo Zoppetti <angelo@MacBook-Air-de-Angelo.local>
2025-09-27 09:36:45 -0300

    Scrum 12 nuevos modelos para cartas y secretos + actualizacion de otros modelos

--- src/database/models.py
+++ src/database/models.py
@@ -9,10 +9,14 @@ class Game(Base):
     __tablename__ = 'games' 
     game_id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(15), nullable=False)
-    status = Column(String(50), default='esperando jugadores') # 'esperando jugadores', 'en curso', 'finalizada'
+    status = Column(String(50), default='waiting players') # 'esperando jugadores', 'en curso', 'finalizada'
     max_players = Column(Integer, nullable=False)
     min_players = Column(Integer, nullable=False)
     players = relationship("Player", back_populates="game")
+    players_amount = Column(Integer,nullable = False)
+    current_turn = Column(Integer, nullable = True)
+    cards = relationship("Card", back_populates="game")
+    secrets = relationship("Secrets", back_populates="game")
 
 class Player(Base):
     __tablename__ = 'players'
@@ -22,24 +26,29 @@ class Player(Base):
     birth_date = Column(Date, nullable = False)
     game_id = Column(Integer, ForeignKey("games.game_id"), nullable=False)  
     game = relationship("Game", back_populates="players")
+    turn_order = Column(Integer) # Posición del jugador en el turn
 
 class Card(Base):
-    __tablename__ = 'cards'
+    _tablename_ = 'cards'
     card_id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(String(15))
     picked_up = Column(Boolean)
     dropped = Column(Boolean)
-    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)  
+    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)
     player = relationship("Players", back_populates="cards")
+    game_id = Column(Integer, ForeignKey("games.game_id"), nullable=False)  
+    game = relationship("Game", back_populates="cards")
 
 class Secrets(Base):
-    __tablename__ = 'Secrets'
+    _tablename_ = 'secrets'
     secret_id = Column(Integer, primary_key=True, autoincrement=True)
     murderer = Column(Boolean)
     acomplice = Column(Boolean)
     revelated = Column(Boolean)
     player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)  
     player = relationship("Players", back_populates="players")
+    game_id = Column(Integer, ForeignKey("games.game_id"), nullable=False)  
+    game = relationship("Game", back_populates="secrets")
 
     
    
