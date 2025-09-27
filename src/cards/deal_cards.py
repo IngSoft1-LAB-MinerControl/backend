f675f784c451c02a18de5f1e8bccca16fc7dd252
Valentino Rimada <valentino.rimada@mi.unc.edu.ar>
2025-09-27 13:42:04 -0300

    SCRUM-18 agrego validacion para que no se repartan dos veces

--- src/cards/deal_cards.py
+++ src/cards/deal_cards.py
@@ -13,6 +13,9 @@ def deal_cards_to_players(game_id: int, db: Session):
 
     # Obtener todas las cartas disponibles (las que no tienen un player_id asignado).
     deck = db.query(Card).filter(Card.game_id == game_id, Card.player_id == None).all()
+    if len(deck) < 55: 
+        raise HTTPException(status_code=400, detail="las cartas ya han sido repartidas en esta partida.")
+
     # se supone que esto se llama cuando arranca la partida asiq todo va a estar en None
 
     #se podria chequear con la cantidad de cartas y ver que el tamano de la lista 
