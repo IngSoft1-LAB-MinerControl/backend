a8f8e8f85c0fc8fb5b20d61a8a9d03237cdde91c
Luca Maccioni <lucamaccioni57@icloud.com>
2025-09-28 01:37:09 -0300

    SCRUM-20 : arreglo de bug de sintaxis

--- src/routes/secrets_routes.py
+++ src/routes/secrets_routes.py
@@ -8,7 +8,7 @@ secret = APIRouter()
 @secret.get("/lobby/secrets/{player_id}")
 def list_secrets_of_player(player_id : int , db: Session = Depends(get_db)):
     secrets = db.query(Secrets).filter(Secrets.player_id == player_id).all() # .all() me devuelve una lista, si no hay nada devuelve lista vacia
-    if not cards:
+    if not secrets:
         raise HTTPException(status_code=404, detail="No secrets found for the given game_id")
-    return cards
+    return secrets
 
