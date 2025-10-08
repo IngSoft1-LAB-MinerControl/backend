9e5b98daff29959d8eb824447c7451db6bade772
Angelo Zoppetti <angelo@MacBook-Air-de-Angelo.local>
2025-10-08 10:14:01 -0300

    Scrum 45 nuevas implementaciones para los card response a usar en el front + modificaciones en los tests

---
+++ src/schemas/card_schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional 
+
+class Card_Response(BaseModel): 
+    card_id : int
+    player_id : Optional[int] = None
+    game_id : int
+    picked_up : bool
+    dropped : bool
+    type : str
+    class config:
+        orm_mode = True
