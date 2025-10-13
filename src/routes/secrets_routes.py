93ea8129de723bd64191ae7149965d64f80935dc
Valentino Rimada <valentino.rimada@mi.unc.edu.ar>
2025-10-13 09:59:24 -0300

    SCRUM-59 resuelto asignar asesino y complice

--- src/routes/secrets_routes.py
+++ src/routes/secrets_routes.py
@@ -17,3 +17,10 @@ def list_secrets_of_player(player_id : int , db: Session = Depends(get_db)):
     
     return secrets
 
+@secret.get("/lobby/secrets_game/{game_id}", tags = ["Secrets"] , response_model= list[Secret_Response])
+def list_secrets_of_game(game_id : int , db: Session = Depends(get_db)):
+    secrets = db.query(Secrets).filter(Secrets.game_id == game_id).all()
+    if not secrets:
+        raise HTTPException(status_code=404, detail="No secrets found for the given game_id")
+    
+    return secrets
